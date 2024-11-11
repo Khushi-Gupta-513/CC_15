eb6730d1708a7416edab57c7b7e9086b0bbfcdfd
Khushi-Gupta-513 <khushigupta050103@gmail.com>
2024-11-10 20:39:13 -0500

    Create asset module with asset list and getAssetById function.

--- asset.js
+++ asset.js
@@ -1,1 +1,13 @@
+// asset.js
+const assets = [
+    { id: 1, name: "Apple Inc.", type: "stock", price: 150, quantity: 10 },
+    { id: 2, name: "US Treasury Bond", type: "bond", price: 1000, quantity: 5 },
+    { id: 3, name: "Tesla Inc.", type: "stock", price: 600, quantity: 7 },
+];
+
+function getAssetById(id) {
+    return assets.find(asset => asset.id === id);
+}
+
+export { assets, getAssetById };
 
