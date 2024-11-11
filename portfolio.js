4ff3d0dacfed18afb7de95a9421664f47079e3d0
Khushi-Gupta-513 <khushigupta050103@gmail.com>
2024-11-10 20:40:33 -0500

    Create portfolio module with calculatePortfolioValue and getPortfolioAllocation functions.

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,17 @@
+// portfolio.js
+import { assets } from './asset.js';
+
+function calculatePortfolioValue() {
+    return assets.reduce((total, asset) => total + (asset.price * asset.quantity), 0);
+}
+
+function getPortfolioAllocation() {
+    const totalValue = calculatePortfolioValue();
+    return assets.map(asset => ({
+        name: asset.name,
+        allocation: ((asset.price * asset.quantity) / totalValue) * 100
+    }));
+}
+
+export { calculatePortfolioValue, getPortfolioAllocation };
 
