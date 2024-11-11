7d857afeb806082ab054ece3eff53b153994d6ec
Khushi-Gupta-513 <khushigupta050103@gmail.com>
2024-11-10 20:42:15 -0500

    Create transaction module with Transaction class.

--- transaction.js
+++ transaction.js
@@ -1,1 +1,30 @@
+// transaction.js
+import { assets, getAssetById } from './asset.js';
+
+class Transaction {
+    constructor(assetId, type, quantity) {
+        this.assetId = assetId;
+        this.type = type;
+        this.quantity = quantity;
+        this.executeTransaction();
+    }
+
+    executeTransaction() {
+        const asset = getAssetById(this.assetId);
+        if (!asset) throw new Error("Asset not found");
+
+        if (this.type === "buy") {
+            asset.quantity += this.quantity;
+        } else if (this.type === "sell") {
+            if (asset.quantity < this.quantity) {
+                throw new Error(`Insufficient quantity for sale of ${asset.name}`);
+            }
+            asset.quantity -= this.quantity;
+        } else {
+            throw new Error("Transaction type must be 'buy' or 'sell'");
+        }
+    }
+}
+
+export { Transaction };
 
